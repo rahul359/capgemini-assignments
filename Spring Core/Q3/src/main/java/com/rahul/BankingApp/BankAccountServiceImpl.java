85191163e5ceb1699de90da6542c44aa4cb6c613
rahul359 <48013460+rahul359@users.noreply.github.com>
2021-03-31 17:04:45 +0530

    Update BankAccountServiceImpl.java

--- Spring Core/Q3/src/main/java/com/rahul/BankingApp/BankAccountServiceImpl.java
+++ Spring Core/Q3/src/main/java/com/rahul/BankingApp/BankAccountServiceImpl.java
@@ -1,28 +1,28 @@
-package com.vunnam.BankingApp;
+package com.rahul.BankingApp;
 
 public class BankAccountServiceImpl implements BankAccountService {
 
 	@Override
 	public double withdraw(long accountId, double balance) {
-		// TODO Auto-generated method stub
+	
 		return 0;
 	}
 
 	@Override
 	public double deposit(long accountId, double balance) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public double getBalance(long accountId) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public boolean fundTransfer(long fromAccount, long toAccount, double amont) {
-		// TODO Auto-generated method stub
+	
 		return false;
 	}
 
