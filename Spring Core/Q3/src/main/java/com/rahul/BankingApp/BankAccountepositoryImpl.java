59858c3c4d362a627d8df29e679768372aff4a9c
rahul359 <48013460+rahul359@users.noreply.github.com>
2021-03-31 17:05:20 +0530

    Update BankAccountepositoryImpl.java

--- Spring Core/Q3/src/main/java/com/rahul/BankingApp/BankAccountepositoryImpl.java
+++ Spring Core/Q3/src/main/java/com/rahul/BankingApp/BankAccountepositoryImpl.java
@@ -1,16 +1,16 @@
-package com.vunnam.BankingApp;
+package com.rahul.BankingApp;
 
 public class BankAccountepositoryImpl implements BankAccountRepository {
 
 	@Override
 	public double getBalance(long accountId) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public double updateBalance(long accountId, double newBalance) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
